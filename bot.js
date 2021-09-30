51e563265fd9692f5ada2cefb956fad08d141b1f
fenderblenders <91694335+fenderblenders@users.noreply.github.com>
2021-09-30 21:22:46 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js"); //TÜM TELİF HAKKI ENES ACARA AİTTİR youtube.com/enesacaryt KANALINDA VİDEOLU ANLATIMI BULUNMAKTADIR
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const { Client, Util } = require("discord.js");
+const request = require("request");
+client.login(ayarlar.token);
+
+
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+   
+
+const http = require("http");
+const express = require("express");
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) { 
+    if ((new Date().getTime() - start) > milliseconds){         //// 7-24 AÇIK KALMASINI SAĞLAYAN KOMUT BİRŞEY DEĞİŞTİRMEYİN
+      break;
+    }
+  }    
+}
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır."); 
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);   //// 7-24 AÇIK KALMASINI SAĞLAYAN KOMUT BİRŞEY DEĞİŞTİRMEYİN
+}, 3000);
